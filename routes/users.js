6997f8ce5b3c6eb6ef0c411fdae9c8ea900ad476
SUNKARA VENKATA NAVEEN <40927874+venkat3697@users.noreply.github.com>
2024-10-14 14:05:24 +0530

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 const { RegisterUser, LoginUser } = require('../controllers/usermanagement');
+const { CreateEvent, getEventsByUserId,getEventsById } = require('../controllers/events');
 var router = express.Router();
 
 /* GET users listing. */
 router.post('/register', RegisterUser);
 router.post('/login', LoginUser);
+router.post('/event',CreateEvent)
+router.get('/event/:eventId',getEventsById)
+router.get('/userevents/:userId',getEventsByUserId)
+
 
 module.exports = router;
